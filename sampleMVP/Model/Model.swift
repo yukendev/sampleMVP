5594f1526e09a41dbce120da400b2876c587f556
yuken <yuken0906dev@gmail.com>
2022-02-06 10:52:47 +0900

    init

---
+++ sampleMVP/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  sampleMVP
+//
+//  Created by 手塚友健 on 2022/02/06.
+//
+
+import Foundation
---
+++ sampleMVP/Presenter/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  sampleMVP
+//
+//  Created by 手塚友健 on 2022/02/06.
+//
+
+import Foundation
